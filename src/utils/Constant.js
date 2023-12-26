29edc4a79f7dd0fbd40a05ec8034587e71838150
ahmar7 <ahmarjabbar7@gmail.com>
2023-12-26 15:29:10 +0500

    upp

---
+++ src/utils/Constant.js
@@ -0,0 +1,11 @@
+module.exports = {
+  // Local server
+  // baseUrl: "http://localhost:4000/api/v1",
+  // Local server
+  // My vercel server
+  baseUrl: "https://blockchain-server-funk.onrender.com/api/v1",
+  // My vercel server
+  // Official server
+  // baseUrl: "https://blockchain-servertx.onrender.com/api/v1",
+  // Official server
+};
